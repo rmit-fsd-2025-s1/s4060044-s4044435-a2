2e46a94cca9d72dab6e8ce863005c18b4f63de8a
AJ <s4060044@student.rmit.edu.au>
2025-06-07 21:55:23 +1000

    basic testing setup

---
+++ admin/Backend/jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  rootDir: './',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
+
+export default config;
